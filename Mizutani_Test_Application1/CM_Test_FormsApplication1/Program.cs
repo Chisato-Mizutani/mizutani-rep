b8234480d5b1bd550ed8c6329f153ebc1e82c6f7
Chisato Mizutani <igarashi@upward-hd.com>
2015-05-04 21:48:43 +0900

    test
    test

---
+++ Mizutani_Test_Application1/CM_Test_FormsApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CM_Test_FormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
